823e359443534b2d05c732f5945b4dd1200d6ffd
Rubbish-Rabbit <1305237417@qq.com>
2019-03-27 11:51:49 +0800

    perfomance

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,15 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list :cities="cities" :hot="hotCities"></city-list>
-    <city-alphabet :cities="cities"></city-alphabet>
+    <city-list
+      :cities="cities"
+      :hot="hotCities"
+      :letter="letter"
+    ></city-list>
+    <city-alphabet
+      :cities="cities"
+      @change="handleLetterChange"
+    ></city-alphabet>
   </div>
 </template>
 
@@ -13,32 +20,37 @@
   import CitySearch from './components/Search'
   import CityList from './components/List'
   import CityAlphabet from './components/Alphabet'
+
   export default {
-    name:'City',
-    components:{
+    name: 'City',
+    components: {
       CityHeader,
       CitySearch,
       CityList,
       CityAlphabet
     },
-    data(){
-      return{
-        cities:{},
-        hotCities:[]
+    data() {
+      return {
+        cities: {},
+        hotCities: [],
+        letter: []
       }
     },
-    methods:{
-      getCityinfo(){
+    methods: {
+      getCityinfo() {
         axios.get('/api/city.json')
           .then(this.handleGetCityInfoSucc)
       },
-      handleGetCityInfoSucc(res){
-        res=res.data
-        if(res.ret && res.data){
+      handleGetCityInfoSucc(res) {
+        res = res.data
+        if (res.ret && res.data) {
           const data = res.data
           this.cities = data.cities
           this.hotCities = data.hotCities
         }
+      },
+      handleLetterChange(letter) {
+        this.letter = letter
       }
     },
     mounted() {
